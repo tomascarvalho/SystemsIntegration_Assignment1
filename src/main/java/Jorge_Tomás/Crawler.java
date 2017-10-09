4e8258d7ef03c574d3700cf9729b521b5affd448
Tomás Carvalho <tomcarvalhoc12@gmail.com>
2017-10-09 12:07:39 +0100

    Crawler: Add crawler class

---
+++ src/main/java/Jorge_Tomás/Crawler.java
@@ -0,0 +1,17 @@
+package Jorge_Tomás;
+
+/**
+ * Created by tomas on 09/10/2017.
+ */
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+
+public class Crawler {
+
+}
